97be5d192d8a9fe7e2b3bf118a12af3adc5d595c
JoshuaFowler <jfowler8912@gmail.com>
2023-03-25 18:30:48 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective (param1 = '*') {
+    return function innerFunction(parameter = "special"){
+        return `You are ${param1}${parameter}${param1}!`
+    }
+}
+
+// const egregiouslyNestedArray = [
+//     "How",
+//     ["deep", ["can", ["we", ["go", ["?"], "Pretty"], "dang"], "deep,"], "it"],
+//     "seems.",
+//   ];
